0109fe2dd962558cd67a2ecacf238857cd3279a5
Subin <subin.viswanathan@ness.com>
2020-05-26 12:53:01 +0530

    adding the giard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { NgbModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -19,6 +19,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { environment } from 'src/environments/environment';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -46,11 +47,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
       { path: 'login', component: LoginComponent },
       { path: 'my-order', component: MyorderComponent },
       { path: 'products', component: ProductComponent },
+
       { path: 'shopping-cart', component: ShoppingCartComponent },
-      { path: 'checkout', component: CheckoutComponent },
-      { path: 'success', component: OrderSuccessComponent },
-      { path: 'admin/orders', component: AdminOrderComponent },
-      { path: 'admin/products', component: AdminProductComponent },
+      { path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard] },
+      { path: 'success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
+      
+      { path: 'admin/orders', component: AdminOrderComponent, canActivate: [AuthGuard] },
+      { path: 'admin/products', component: AdminProductComponent, canActivate: [AuthGuard] },
       { path: '**', component: NotFoundComponent }
     ])
   ],
---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private _auth: AuthService, private _router: Router) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this._auth.user$
+      .pipe(
+        map(user => {
+          if (user) return true;
+
+          this._router.navigate(['/login']);
+        })
+      )
+  }
+}
