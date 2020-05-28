71d2ea9eac15d8ea050b8b82eabd725145c34c26
Subin <subin.viswanathan@ness.com>
2020-05-28 14:13:54 +0530

    implement search functionality

--- src/app/admin/admin-product/admin-product.component.ts
+++ src/app/admin/admin-product/admin-product.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-product',
   templateUrl: './admin-product.component.html',
   styleUrls: ['./admin-product.component.css']
 })
-export class AdminProductComponent implements OnInit {
+export class AdminProductComponent implements OnInit, OnDestroy {
 
-  productList$;
+  products = [];
+  filteredProducts = [];
+  subscription: Subscription;
 
   constructor(private _productService: ProductService) {
-    this.productList$ = _productService.getAll()
+    this.subscription = _productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   ngOnInit() {
+  }
+
+  filter(query: string) {
+    this.filteredProducts = query ?
+      this.products.filter(product => product.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
   }
 
 }
